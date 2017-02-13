ffbbd02170b3085be1cb5dbc4488fdd39fd19695
“therealzac” <Wickstrom913@gmail.com>
2017-02-13 11:27:42 -0800

    progress

---
+++ src/client/utils/configureStore.js
@@ -0,0 +1,28 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import { routerMiddleware } from 'react-router-redux';
+import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
+import rootReducer from '../reducers/index';
+
+/*
+ * @param {Object} initial state to bootstrap our stores with for server-side rendering
+ * @param {History Object} a history object. We use `createMemoryHistory` for server-side rendering,
+ *                          while using browserHistory for client-side
+ *                          rendering.
+ */
+const configureStore = function (initialState, history) {
+  // Installs hooks that always keep react-router and redux store in sync
+  const middleware = [thunk, routerMiddleware(history)];
+  let store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(...middleware),
+      window.devToolsExtension ? window.devToolsExtension() : f => f
+    )
+  );
+
+  return store;
+}
+
+module.exports = configureStore;
